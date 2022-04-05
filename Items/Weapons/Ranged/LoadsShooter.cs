e13882bcffde1e738c1a6300887256392b2f339c
Saul Budie <101558088+SaulBudie@users.noreply.github.com>
2022-04-05 18:57:49 +0700

    Add files via upload

---
+++ Items/Weapons/Ranged/LoadsShooter.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Tietank.Items.Weapons.Ranged
+{
+	public class LoadsShooter : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Loads Shooter");
+			string a = "'Quite durable isn't it?'\nShoots extra white sprays";
+			base.Tooltip.SetDefault(a);
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 24;
+			item.height = 24;
+			item.value = Item.sellPrice(0, 1, 0 ,0);
+			item.rare = ItemRarityID.LightRed;
+			item.shoot = 1;
+			item.crit = 10;
+			item.knockBack = 6.5f;
+			item.useStyle = 5;
+			item.useAnimation = 32;
+			item.useTime = 32;
+			item.autoReuse = true;
+			item.useAmmo = AmmoID.Bullet;
+			item.UseSound = SoundID.Item36;
+			item.damage = 31;
+			item.shootSpeed = 7.6f;
+			item.noMelee = true;
+			item.ranged = true;
+		}
+
+        public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-2f, -6f);
+		}
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+			for (int i = 0; i < 5; i++)
+
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(9)); 
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y,type, damage, knockBack, player.whoAmI);
+			}
+			for (int i = 0; i < 3; i++)
+
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(14));
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, ModContent.ProjectileType<Projectiles.Ranged.WhiteSprays>(), damage, knockBack, player.whoAmI);
+			}
+			return false;
+		}
+        public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ModContent.ItemType<Items.Weapons.Ranged.NutBlaster>());
+			recipe.AddIngredient(ItemID.HallowedBar, 5);
+			recipe.AddIngredient(ItemID.SoulofMight, 5);
+			recipe.AddTile(TileID.MythrilAnvil);
+
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+
+
+
+		}
+
+
+
+
+
+
+	}
+}
---
+++ Items/Weapons/Ranged/NatureSFuror.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace Tietank.Items.Weapons.Ranged
+{
+	public class NatureSFuror : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Nature's Furor");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 24;
+			item.height = 24;
+			item.value = Item.sellPrice(0, 1, 0 ,0);
+			item.rare = ItemRarityID.Green;
+			item.autoReuse = true;
+			item.shoot = 1;
+			item.crit = 10;
+			item.knockBack = 6.5f;
+			item.useStyle = 5;
+			item.useAnimation = 45;
+			item.useTime = 45;
+			item.useAmmo = AmmoID.Bullet;
+			item.UseSound = SoundID.Item36;
+			item.damage = 34;
+			item.shootSpeed = 7f;
+			item.noMelee = true;
+			item.ranged = true;
+		}
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float scale, int whoAmI)
+		{
+			Lighting.AddLight(item.Center, new Color(1, 255, 11).ToVector3() * 1);
+		}
+
+        public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-2f, -6f);
+		}
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+			for (int i = 0; i < 5; i++)
+
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedByRandom(MathHelper.ToRadians(12)); 
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y,type, damage, knockBack, player.whoAmI); 
+			}
+			Projectile.NewProjectile(position.X, position.Y, speedX, speedY, ModContent.ProjectileType<Projectiles.Ranged.ChlorophyteBolt>(), damage, knockBack, player.whoAmI); 
+			return false;
+		}
+        public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Shotgun, 1);
+			recipe.AddIngredient(ItemID.ChlorophyteBar, 10);
+			recipe.AddIngredient(ItemID.SoulofFright, 6);
+			recipe.AddIngredient(ModContent.ItemType<Loots.PlanteraPetals>() ,5);
+			recipe.AddTile(TileID.MythrilAnvil);
+
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+
+
+
+		}
+
+
+
+
+
+
+	}
+}
